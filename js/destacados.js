152a9267a5c8f3a5807e296f9898d14dbf411ffd
Michior <111088211+Michior@users.noreply.github.com>
2024-11-14 10:27:54 -0600

    Primeras implementaciones de administrar eventos

--- js/destacados.js
+++ js/destacados.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { mostrarConciertos } from "../empleado/js/config.js";
 document.addEventListener("DOMContentLoaded", () => {
     const contenedorCartas = document.getElementById("eventosContainer");
@@ -27,3 +28,34 @@ document.addEventListener("DOMContentLoaded", () => {
     mostrarTarjetasConciertos();
 
 });
+=======
+import { mostrarConciertos } from "../empleado/js/config.js";
+document.addEventListener("DOMContentLoaded", () => {
+    const contenedorCartas = document.getElementById("eventosContainer");
+    const mostrarTarjetasConciertos = async () => {
+        try {
+            const arregloConcierto = await mostrarConciertos();
+            arregloConcierto.forEach(concierto => {
+                const conciertoCard = document.createElement("article");
+                conciertoCard.innerHTML = `
+            <img src="${concierto.urlImagen}" alt=""${concierto.artista}">
+            <h3>${concierto.artista} </h3>
+            <p>${concierto.descripcion}
+            <span>Fecha: ${new Date(concierto.fecha).toLocaleDateString()}</span>
+            </p>
+            <a href="detallesCompra.html" class="button"> Comprar boleto</a>
+            `;
+
+                contenedorCartas.appendChild(conciertoCard);
+
+            });
+        }
+        catch (error) {
+            alert("No se pueden mostrar los concietos: ", error)
+        }
+    }
+
+    mostrarTarjetasConciertos();
+
+});
+>>>>>>> 255c0f5 (merge con nat)
