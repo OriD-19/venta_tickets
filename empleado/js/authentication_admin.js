d4736063c357575aaae303e4ced6cc0d3fdfea1f
Fer Fuentes <fernandocastillo.8@hotmail.com>
2024-11-14 18:02:07 -0600

    feature: sistema de usuarios CASI CASI completado, de nuevo

--- empleado/js/authentication_admin.js
+++ empleado/js/authentication_admin.js
@@ -1,9 +1,10 @@
 if (sessionStorage.getItem("user") === null) {
-    alert('what');
-    window.location.href = "index.html";
+    alert("No has iniciado sesión");
+    window.location.href = "login.html";
 }
 
 const user = JSON.parse(sessionStorage.getItem("user"));
 if (user.role !== "admin") {
+    alert("No tienes permisos para acceder a esta página");
     window.location.href = "inicio_empleado.html";
 }
